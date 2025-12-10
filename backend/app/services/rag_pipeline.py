cefff96b507d1022c4e539a84b2ba52a381e6d8f
Ayush Tiwari <ayushzztiwari69@gmail.com>
2025-12-10 12:10:11 +0530

    set up qdrant DB

--- backend/app/services/rag_pipeline.py
+++ backend/app/services/rag_pipeline.py
@@ -15,7 +15,8 @@ embedder = SentenceTransformer("all-MiniLM-L6-v2")
 # ---------------------------
 # 2. Connect to Qdrant
 # ---------------------------
-qdrant = QdrantClient(url="http://localhost:6333")
+qdrant = QdrantClient(url="" + os.getenv("QDRANT_URL", "http://localhost:6333"),
+                      api_key=os.getenv("QDRANT_API_KEY", None))
 
 COLLECTION_NAME = "documents"
 
