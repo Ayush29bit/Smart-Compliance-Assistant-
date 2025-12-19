f326472b0af42f7c68e98967acb9494d8d3ca21a
Ayush Tiwari <ayushzztiwari69@gmail.com>
2025-12-19 12:42:11 +0530

    embedder.py updated for better modularity

--- backend/app/services/embedder.py
+++ backend/app/services/embedder.py
@@ -1,6 +1,23 @@
 from sentence_transformers import SentenceTransformer
 
-embedder = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
-def get_embedding(text: str):
-    return embedder.encode(text).tolist()
+def chunk_and_embed(text: str, chunk_size=300):
+    words = text.split()
+    chunks = []
+    current = []
+
+    for word in words:
+        current.append(word)
+        if len(current) >= chunk_size:
+            chunk_text = " ".join(current)
+            embedding = model.encode(chunk_text).tolist()
+            chunks.append((chunk_text, embedding))
+            current = []
+
+    if current:
+        chunk_text = " ".join(current)
+        embedding = model.encode(chunk_text).tolist()
+        chunks.append((chunk_text, embedding))
+
+    return chunks
